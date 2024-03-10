963800920e6bfca60025fbca34393cd6390b9b10
SachinDJ04 <sachinpatidradj04@gmail.com>
2024-03-10 12:37:01 +0530

    Init

---
+++ BugTrackingApplicationAPIs/src/main/java/com/app/tracking/bug/BugTrackingApplicationApIsApplication.java
@@ -0,0 +1,13 @@
+package com.app.tracking.bug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackingApplicationApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackingApplicationApIsApplication.class, args);
+	}
+
+}
---
+++ BugTrackingApplicationAPIs/src/test/java/com/app/tracking/bug/BugTrackingApplicationApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tracking.bug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackingApplicationApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
